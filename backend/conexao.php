97a5d0782a0b8bb9e13db9800fb3617fca9798f3
Andre Tres <andretres544@gmail.com>
2025-04-27 16:30:02 -0300

    Update conexao.php

--- backend/conexao.php
+++ backend/conexao.php
@@ -1,1 +1,15 @@
+<?php
+// Configurações de conexão com o banco de dados
+$servername = "localhost"; // Geralmente 'localhost' para o WAMP
+$username = "root"; // Usuário padrão do MySQL no WAMP
+$password = ""; // Senha padrão do MySQL no WAMP (vazio por padrão)
+$dbname = "form_contato"; // Nome do banco de dados
 
+// Criando a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificando a conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+?>
